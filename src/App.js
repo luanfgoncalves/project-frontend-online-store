71daff2394f2222bbe5fa86f8042b68fc247c780
J. Gouveia <gouvik.dev@gmail.com>
2022-07-07 19:17:09 -0300

    build: Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import './App.css';
-import getCategories from './services/api';
+// import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
 class App extends React.Component {
-  componentDidMount() {
-    console.log('montou App');
-    console.log(getCategories());
-  }
-
-  componentDidUpdate() {
-    console.log('atualização App');
-    console.log(getCategories());
-  }
-
   render() {
     return (
-      <h1>{ console.log(getCategories()) }</h1>
+      <h1>Hello World!</h1>
     );
   }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,21 @@
-export default async function getCategories() {
+export async function getCategories() {
   const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
   const response = await fetch(URL);
   const data = await response.json();
   return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (categoryId) {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+  if (query) {
+    const urlQuery = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+    const response = await fetch(urlQuery);
+    const data = await response.json();
+    return data;
+  }
 }

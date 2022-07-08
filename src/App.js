d520e94c3b78aab89963d193648cb765af83e40a
J. Gouveia <gouvik.dev@gmail.com>
2022-07-08 11:22:30 -0300

    build: req 03

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import ShopCartComponent from './pages/ShopCart';
 // import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
 class App extends React.Component {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route path="/" component={ Home } exact />
+          <Route exact path="/shopcart" component={ ShopCartComponent } />
         </Switch>
       </BrowserRouter>
     );
--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
+const URL = (endpoint) => `https://api.mercadolibre.com/sites/MLB/${endpoint}`;
+
 export async function getCategories() {
-  const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
-  const response = await fetch(URL);
+  const response = await fetch(URL('categories'));
   const data = await response.json();
   return data;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   if (categoryId) {
-    const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
-    const response = await fetch(url);
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
     const data = await response.json();
     return data;
   }
@@ -17,5 +17,10 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
     const response = await fetch(urlQuery);
     const data = await response.json();
     return data;
+  }
+  if (categoryId && query) {
+    const request = fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+    const response = await request.json();
+    return response;
   }
 }

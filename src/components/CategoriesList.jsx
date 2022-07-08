7309c5ec60312aaa6487ef2bbbb845ece519f072
luanfgoncalves <luanfgoncalves@outlook.com>
2022-07-08 16:32:29 -0300

    BUILD: Req 4; FEAT: CategoriesList.jsx e App.css

--- src/components/CategoriesList.jsx
+++ src/components/CategoriesList.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class CategoriesList extends React.Component {
   render() {
+    const { apiCategoriesProp } = this.props;
     return (
-      <aside>
-        <p>Categorias:</p>
-        <form>
-            
-        </form>
-      </aside>
+      <div>
+        { apiCategoriesProp.map(({ name, id }) => (
+          <label htmlFor="listItem" data-testid="category" key={ id }>
+            <input type="radio" id="listItem" />
+            { name }
+          </label>)) }
+      </div>
     );
   }
 }
+
+CategoriesList.propTypes = {
+  apiCategoriesProp: PropTypes.arrayOf(Object).isRequired,
+};
 
 export default CategoriesList;

1d639392ac2cccf0bd16b4c2f233b5595fb29141
luanfgoncalves <luanfgoncalves@outlook.com>
2022-07-08 14:41:49 -0300

    Feat: Primeiro componente - CategoriesList.jsx

---
+++ src/components/CategoriesList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class CategoriesList extends React.Component {
+  render() {
+    return (
+      <aside>
+        <p>Categorias:</p>
+        <form>
+            
+        </form>
+      </aside>
+    );
+  }
+}
+
+export default CategoriesList;

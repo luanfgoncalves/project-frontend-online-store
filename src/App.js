30dc2716f610743b4ee5d351aad42e3db4d5a32e
J. Gouveia <gouvik.dev@gmail.com>
2022-07-11 16:54:08 -0300

    repo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import ShopCartComponent from './pages/ShopCart';
+import ProductDetailsCard from './pages/ProductDetails';
 // import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
 class App extends React.Component {
@@ -10,8 +11,13 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ Home } exact />
+          <Route exact path="/" component={ Home } />
           <Route exact path="/shopcart" component={ ShopCartComponent } />
+          <Route
+            exact
+            path="details:id"
+            render={ (props) => <ProductDetailsCard { ...props } /> }
+          />
         </Switch>
       </BrowserRouter>
     );
